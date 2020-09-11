083a3dc2c6766e39f9badc2754a41f0d67c7b615
furkanaydar <furkanaydar97@gmail.com>
2020-09-11 06:29:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const http = require('http');
 //import app.js file
 const app = require('./app__');
 
-const port = 3101;
+const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    //    let's print a message when the server run successfully
-    console.log("Server restarted successfully")
+    console.log("App is running on port " + port);
 });
